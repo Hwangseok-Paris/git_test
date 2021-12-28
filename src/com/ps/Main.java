341f064dadb49b9961c78c735d6c9ef59063e342
Hwangseok-Paris <hwangseok.paris@gmail.com>
2021-12-28 11:49:21 +0900

    Initial commit

---
+++ src/com/ps/Main.java
@@ -0,0 +1,18 @@
+package com.ps;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String ab = "하이";
+        int c = 123;
+
+
+        System.out.println("hello I'm Paris");
+        System.out.println(c);
+        System.out.println("Main.main");
+
+    }
+}
